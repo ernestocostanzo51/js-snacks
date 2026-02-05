deb1ee97819bea35f242a0b32773aba54c429930
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-05 11:19:04 +0100

    .

--- js-snacks-blocco-1/main.js
+++ js-snacks-blocco-1/main.js
@@ -3,19 +3,19 @@
 //SE il primo numero è maggiore al secondo pusho il primo
 //ALTRIMENTI metto il secondo numero come primo
 
-let numeri=[]
-//inserisco variabile temporanea (metodo bubble sort)
-let sort
+let numero_1= prompt("inserisci il primo numero")
+let numero_2 = prompt("inserisci il secondo numero")
 
-for(i=0; i<2; i++){
-    prompt("inserisci un numero")
-    numeri.push(numeri)
+
+if(numero_1>numero_2){
+    console.log("il primo numero è il piu grande", numero_1)
+}
+else if(numero_2>numero_1){
+    console.log("il secondo numero è il più grande", numero_2)
+}
+else{
+    console.log("i numeri sono uguali", numero_1, numero_2 )
 }
 
-if(numeri[0]>numeri[1]){
-   sort = numeri[0]
-   numeri[0] = numeri[1]
-   numeri[1] = sort
-}
 
-console.log(numeri)
+
