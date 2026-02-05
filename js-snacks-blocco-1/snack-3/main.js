b9975b8ae63fd7b733ddb9da5a6c456e961b96fe
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-05 12:20:29 +0100

    snack-3 finito

---
+++ js-snacks-blocco-1/snack-3/main.js
@@ -0,0 +1,13 @@
+let numeri=[]
+let somma=0
+
+for(i=0; i<10; i++){
+    let numero =parseInt(prompt("inserisci il numero",))
+    numeri.push(numero)
+    somma += numero
+    
+}
+
+
+console.log(somma)
+
