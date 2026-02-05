e6ca546cdd64bc2e7b82db876f3088b9a354cb1c
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-05 10:37:18 +0100

    first commit

---
+++ js-snacks-blocco-1/main.js
@@ -0,0 +1,11 @@
+//faccio inserire all'utente i numeri tramite un ciclo for
+//tramite un'IF ELSE comparo i due numeri
+//SE il primo numero è maggiore al secondo pusho il primo
+//ALTRIMENTI metto il secondo numero come primo
+
+let numeri=[]
+
+for(i=0; i<2; i++){
+    prompt(numeri)
+    numeri.push
+}
